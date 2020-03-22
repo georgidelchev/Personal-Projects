1aa778ffecd1c2350f5d4046a7c3dcf5042a85dc
georgidelchev <alienguymc@gmail.com>
2020-03-22 18:51:26 +0200

    upload

---
+++ CoronaVirus-StayAtHome/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StayAtHome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you at home?");
+            string areYouAtHome = Console.ReadLine();
+
+            while (areYouAtHome != "Yes".ToLower())
+            {
+                Console.WriteLine("Go to home!");
+                Console.WriteLine("Are you at home now?");
+                areYouAtHome = Console.ReadLine();
+            }
+            Console.WriteLine("Good! Stay there :)!");
+        }
+    }
+}
