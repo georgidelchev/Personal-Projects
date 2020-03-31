e434c810259f65f8c43390dade3978c0f1cd5a3c
georgidelchev <alienguymc@gmail.com>
2020-04-01 02:54:23 +0300

    up

---
+++ Cars-Info-And-Some-Models/carsClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarsNFO
+{
+    class carsClass
+    {
+        public void Mercedes()
+        {
+            string[] modelsMercedes = { "C-Class", "G-Class", "AMG", "GLC", "GLE" };
+            string[] mercedesInfo = {"Mercedes-Benz is a German global automobile marque and a division of Daimler AG. " +
+                    "Mercedes-Benz is known for luxury vehicles, vans, trucks, buses, coaches and ambulances. The headquarters is in Stuttgart, Baden-Württemberg. " +
+                    "The name first appeared in 1926 under Daimler-Benz. In 2018, Mercedes-Benz was the largest seller of premium vehicles in the world, having sold 2.31 million passenger cars. " +
+                    "Mercedes-Benz origins' come from Daimler-Motoren-Gesellschaft's 1901 Mercedes and Karl Benz's 1886 Benz Patent-Motorwagen, which is widely regarded as the first gasoline-powered automobile. " +
+                    "The slogan for the brand is the best or nothing."};
+        }
+        
+    }
+}
