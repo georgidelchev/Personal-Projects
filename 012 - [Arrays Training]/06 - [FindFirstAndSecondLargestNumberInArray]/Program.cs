e61d1ec8361a2781057ead48cfe26c8910155e91
georgidelchev <alienguymc@gmail.com>
2020-05-15 16:49:54 +0300

    add files via upload

---
+++ 012 - [Arrays Training]/06 - [FindFirstAndSecondLargestNumberInArray]/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FindSecondLargestNumberInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size of array: ");
+            int arrSize = int.Parse(Console.ReadLine());
+            int[] arr = new int[arrSize];
+
+            Console.WriteLine($"Enter {arrSize} elements in the array");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                arr[i] += numbers;
+
+            }
+
+            int firstMax, secondMax;
+            firstMax = secondMax = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > firstMax)
+                {
+                    secondMax = firstMax;
+                    firstMax = arr[i];
+                }
+                else if ((arr[i] > secondMax) && (arr[i] < firstMax))
+                {
+                    secondMax = arr[i];
+                }
+            }
+
+            Console.WriteLine($"First largest = {firstMax}");
+            Console.WriteLine($"Second largest = {secondMax}");
+        }
+    }
+}
