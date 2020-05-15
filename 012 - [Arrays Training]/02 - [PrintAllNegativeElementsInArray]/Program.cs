0dd1adf840f2cf6e984eb35bbe2375742618308f
georgidelchev <alienguymc@gmail.com>
2020-05-15 16:15:23 +0300

    add files via upload

---
+++ 012 - [Arrays Training]/02 - [PrintAllNegativeElementsInArray]/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02_PrintAllNegativeElementsInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size of array: ");
+            int arrayLength = int.Parse(Console.ReadLine());
+            int[] numbers = new int[arrayLength];
+
+            Console.WriteLine($"Enter {arrayLength} elements in the array");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int numbersToAdd = int.Parse(Console.ReadLine());
+                numbers[i] += numbersToAdd;
+            }
+
+            Console.WriteLine();
+            Console.Write("All negative elements in array are : ");
+            foreach (int nums in numbers)
+            {
+                if (nums < 0)
+                {
+                    Console.Write($"{nums}\t");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
