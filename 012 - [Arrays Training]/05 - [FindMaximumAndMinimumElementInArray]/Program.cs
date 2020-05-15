3c8288c959973ae45be4643de41aa25ca820be59
georgidelchev <alienguymc@gmail.com>
2020-05-15 16:31:24 +0300

    add files via upload

---
+++ 012 - [Arrays Training]/05 - [FindMaximumAndMinimumElementInArray]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _05_FindMaximumAndMinimumElementInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size of array: ");
+            int arraySize = int.Parse(Console.ReadLine());
+            int[] arr = new int[arraySize];
+
+            Console.WriteLine($"Enter {arraySize} elements in the array");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                arr[i] += numbers;
+            }
+
+            Console.WriteLine($"Maximum element = {arr.Max()}");
+            Console.WriteLine($"Minimum element = {arr.Min()}");
+        }
+    }
+}
