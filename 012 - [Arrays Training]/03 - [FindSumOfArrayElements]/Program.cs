f6bfd8b39f5e454a2ebfcfdeb75ad41d802e40af
georgidelchev <alienguymc@gmail.com>
2020-05-15 16:15:44 +0300

    add files via upload

---
+++ 012 - [Arrays Training]/03 - [FindSumOfArrayElements]/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace FindSumOfArrayElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size of array: ");
+            int arrayLength = int.Parse(Console.ReadLine());
+            int[] arrayNumbers = new int[arrayLength];
+
+            Console.WriteLine($"Enter {arrayLength} elements in the array");
+            for (int i = 0; i < arrayNumbers.Length; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                arrayNumbers[i] += numbers;
+            }
+            Console.WriteLine($"Sum of all elements of array = {arrayNumbers.Sum()}");
+        }
+    }
+}
