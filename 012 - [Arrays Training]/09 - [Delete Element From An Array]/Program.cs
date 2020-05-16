ef414393e23e9149267ab0baf10d4ac1b9242906
georgidelchev <alienguymc@gmail.com>
2020-05-16 15:16:57 +0300

    add files via upload

---
+++ 012 - [Arrays Training]/09 - [Delete Element From An Array]/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DeleteElementFromAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size of array: ");
+            int arrSize = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Enter {arrSize} elements in the array");
+            int[] arr = new int[arrSize];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter the element position to delete : ");
+            int elementToDelete = int.Parse(Console.ReadLine());
+
+            if (elementToDelete < 0 || elementToDelete > arr.Length)
+            {
+                Console.WriteLine("Invalid position!");
+            }
+            else
+            {
+                for (int i = elementToDelete; i < arrSize - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                arrSize--;
+
+                Console.WriteLine("Elements of array after delete are");
+                for (int i = 0; i < arrSize; i++)
+                {
+                    Console.Write($"{arr[i]}\t");
+                }
+            }
+        }
+    }
+}
