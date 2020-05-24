8cb7ebc6b8f09278ada5921ae9dbc6571a920503
georgidelchev <alienguymc@gmail.com>
2020-05-24 16:54:21 +0300

    add files via upload

---
+++ 012 - [Arrays Training]/10 - [Print All Unique Elements In Array]/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PrintAllUniqueElementsInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrSize = int.Parse(Console.ReadLine());
+            int[] arr = new int[arrSize];
+            int[] arr2 = new int[arrSize];
+
+            Console.WriteLine($"Enter {arrSize} elements in the array");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+                arr[i] = elements;
+            }
+
+            Console.Write("Unique elements in the array are: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isUnique = true;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique)
+                {
+                    Console.Write(arr[i] + "\t");
+                }
+            }
+
+        }
+    }
+}
